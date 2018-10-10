c3c18f108c2e674738c11f04a57919a0f655cc65
Alexandre Capt <acapt@adobe.com>
2018-10-10 15:11:12 +0200

    Trying to debug X-Version

--- src/html.pre.js
+++ src/html.pre.js
@@ -196,6 +196,8 @@ async function pre(payload, action) {
     request: actionReq
   } = action;
 
+  logger.debug(`html-pre.js - Current Fastly version - ${actionReq.headers['X-Version']}`);
+
   try {
     if (!payload.content) {
       logger.debug('html-pre.js - Payload has no resource, nothing we can do');

5f6b14ceab2d94c1d9f2f73bf99c0de19ca6e34b
Alexandre Capt <kptdobe@gmail.com>
2019-03-12 14:28:27 +0100

    Setup linting #38

--- test/smoke/test.hlx.up.js
+++ test/smoke/test.hlx.up.js
@@ -9,83 +9,83 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-/* global describe, it */
+/* global describe, it, before, after */
 const assert = require('assert');
 const $ = require('shelljs');
 
-const { assertHttp, sleep } = require('./utils'); 
+const { assertHttp, sleep } = require('./utils');
 
 const HLX_SMOKE_EXEC = process.env.HLX_SMOKE_EXEC || 'hlx';
+// eslint-disable-next-line no-console
 console.debug(`Running smoke test using: ${HLX_SMOKE_EXEC}`);
 
 describe('project-helix.io renders properly', function suite() {
-    this.timeout(10000);
+  this.timeout(10000);
 
-    let hlxup;
+  let hlxup;
 
-    before(async function before() {
-        hlxup = $.exec(`${HLX_SMOKE_EXEC} up --open false`, {
-            // silent: true,
-            async: true
-        }, (code, stdout, stderr) => {
-            assert.ok(!stdout.includes('[hlx] error'), 'No error message allowed');
-            assert.ok(!stdout.includes('[hlx] warn'), 'No warning message allowed');
-        });
-
-        // wait for server to properly start and hlx build to be completed
-        await sleep(5000);
-    });
-    
-    after(async function after() {
-        hlxup.kill();
-        hlxup = null;
-        await sleep(1000);
+  before(async () => {
+    hlxup = $.exec(`${HLX_SMOKE_EXEC} up --open false`, {
+      // silent: true,
+      async: true,
+    }, (code, stdout) => {
+      assert.ok(!stdout.includes('[hlx] error'), 'No error message allowed');
+      assert.ok(!stdout.includes('[hlx] warn'), 'No warning message allowed');
     });
 
-    it('Root ("/" / index.html) is rendered using project htl scripts', async () => {
-        // test for root request
-        let html = (await assertHttp('http://localhost:3000', 200)).toLowerCase();
+    // wait for server to properly start and hlx build to be completed
+    await sleep(5000);
+  });
 
-        assert.ok(html.includes('<body>'), 'index page is rendered - at least contains a body tag')
-        assert.ok(html.includes('<div class="summary">'), 'nav in index (summary_html.htl) is included via ESI include');
-    });
+  after(async () => {
+    hlxup.kill();
+    hlxup = null;
+    await sleep(1000);
+  });
 
-    it('README.html page is rendered using project htl scripts', async () => {
-        // test specific URL
-        html = (await assertHttp('http://localhost:3000/README.html', 200)).toLowerCase();
+  it('Root ("/" / index.html) is rendered using project htl scripts', async () => {
+    // test for root request
+    const html = (await assertHttp('http://localhost:3000', 200)).toLowerCase();
 
-        assert.ok(html.includes('<body>'), 'README page is rendered - at least contains a body tag');
-        assert.ok(html.includes('<div class="summary">'), 'nav in README (summary_html.htl) is included via ESI include');
+    assert.ok(html.includes('<body>'), 'index page is rendered - at least contains a body tag');
+    assert.ok(html.includes('<div class="summary">'), 'nav in index (summary_html.htl) is included via ESI include');
+  });
 
-    });
+  it('README.html page is rendered using project htl scripts', async () => {
+    // test specific URL
+    const html = (await assertHttp('http://localhost:3000/README.html', 200)).toLowerCase();
 
-    it('index.html page source check', async () => {
-        let html = (await assertHttp('http://localhost:3000/index.html', 200)).toLowerCase();
+    assert.ok(html.includes('<body>'), 'README page is rendered - at least contains a body tag');
+    assert.ok(html.includes('<div class="summary">'), 'nav in README (summary_html.htl) is included via ESI include');
+  });
 
-        // check if logo can be downloaded
-        const match = html.match(/("([^"]|"")*helix_logo.png")/g);
-        assert.equal(match.length, 1, 'helix_logo.png is present in the HTML source');
-        let logoURL = match[0].split('"')[1];
-        if(logoURL.indexOf('./') === 0) {
-            logoURL = logoURL.substring(1);
-        }
+  it('index.html page source check', async () => {
+    const html = (await assertHttp('http://localhost:3000/index.html', 200)).toLowerCase();
 
-        await assertHttp(`http://localhost:3000${logoURL}`, 200);
-    });
+    // check if logo can be downloaded
+    const match = html.match(/("([^"]|"")*helix_logo.png")/g);
+    assert.equal(match.length, 1, 'helix_logo.png is present in the HTML source');
+    let logoURL = match[0].split('"')[1];
+    if (logoURL.indexOf('./') === 0) {
+      logoURL = logoURL.substring(1);
+    }
 
-    it('content assets in index.html check', async () => {
-        let html = (await assertHttp('http://localhost:3000/index.html', 200)).toLowerCase();
+    await assertHttp(`http://localhost:3000${logoURL}`, 200);
+  });
 
-        // check if one asset can be downloaded
-        const reg = /src="(assets\/.*?\.png)"/g
+  it('content assets in index.html check', async () => {
+    const html = (await assertHttp('http://localhost:3000/index.html', 200)).toLowerCase();
 
-        assert.ok(reg.test(html), 'no assets found');
+    // check if one asset can be downloaded
+    const reg = /src="(assets\/.*?\.png)"/g;
 
-        let assetURL = reg.exec(html)[1];
-        if (assetURL.indexOf('/') !== 0) {
-            assetURL = `/${assetURL}`;
-        }
+    assert.ok(reg.test(html), 'no assets found');
 
-        await assertHttp(`http://localhost:3000${assetURL}`, 200);
-    });
+    let assetURL = reg.exec(html)[1];
+    if (assetURL.indexOf('/') !== 0) {
+      assetURL = `/${assetURL}`;
+    }
+
+    await assertHttp(`http://localhost:3000${assetURL}`, 200);
+  });
 });

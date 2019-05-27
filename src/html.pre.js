f86a124e6a2aaa2dcd2afa4976c35bbbbfcad748
Stefan Guggisberg <sguggisb@adobe.com>
2019-05-27 18:52:09 +0200

    build(lint) fix CI build

--- src/html.pre.js
+++ src/html.pre.js
@@ -190,6 +190,7 @@ async function pre(payload, action) {
     } else {
       logger.debug('html-pre.js - No REPO_RAW_ROOT provided');
     }
+    return payload;
   } catch (e) {
     logger.error(`Error while executing html.pre.js: ${e.stack || e}`);
     return {
